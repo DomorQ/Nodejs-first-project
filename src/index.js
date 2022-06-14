227fe1af96a07b3f5f0a7d78a5625bc2be020c35
Jorge Dominguez <domorqfut1998@gmail.com>
2022-06-14 18:21:47 -0500

    view engine ejs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World by Jorge Dominguez")
+import express from 'express'
+import ejs from 'ejs'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.send('Hello world By Jorge Dominguez'))
+
+app.listen(3000)
+console.log('Server in listening on port',3000)
