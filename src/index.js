9a1513318e56a1b39679333046c13f6d180fac76
Jorge Dominguez <domorqfut1998@gmail.com>
2022-06-14 18:54:47 -0500

    __dirname complete path of proyect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express'
-import ejs from 'ejs'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express()
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+console.log(__dirname) // __dirname trae la direccion completa de donde se ubica el proyecto
+
+app.set('views',)
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.send('Hello world By Jorge Dominguez'))
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(3000)
 console.log('Server in listening on port',3000)
