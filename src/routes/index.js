97ebcf4366f463c3a05a93cbdf052b4150463b76
Jorge Dominguez <domorqfut1998@gmail.com>
2022-06-14 20:23:42 -0500

    <%- %> this interprate code

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ import { Router } from 'express' // modulo router ayuda a definir o instanciar d
 
 const router = Router()
 
-router.get('/', (req, res) => res.render('index'))
+router.get('/', (req, res) => res.render('index', {title : 'First Web with Node'})) //al poner {} se envia un objeto para un titulo mas personalizado se define la variable y el contenido
 
-router.get('/about', (req, res) => res.render('about'))
+router.get('/about', (req, res) => res.render('about', {title: 'About me'}))
 
-router.get('/contact', (req, res) => res.render('contact'))
+router.get('/contact', (req, res) => res.render('contact', {title: 'Contact page'}))
 
 export default router
